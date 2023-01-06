c1f58f852f23346ecee1eefccdc71b7e7a575288
uzairmasood88 <uzairmasood88@users.noreply.github.com>
2023-01-06 12:59:43 -0800

    Reservation

---
+++ process_reservation.php
@@ -0,0 +1,37 @@
+<?php
+
+// Connect to the database
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "reservations";
+
+// Create connection
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Escape user inputs for security
+$name = mysqli_real_escape_string($conn, $_POST['name']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$number = mysqli_real_escape_string($conn, $_POST['number']);
+$date = mysqli_real_escape_string($conn, $_POST['date']);
+$time = mysqli_real_escape_string($conn, $_POST['time']);
+$phone = mysqli_real_escape_string($conn, $_POST['phone']);
+$message = mysqli_real_escape_string($conn, $_POST['message']);
+
+// Attempt insert query execution
+$sql = "INSERT INTO reservations (name, email, number, date, time, phone, message) VALUES ('$name', '$email', '$number', '$date', '$time', '$phone', '$message')";
+if(mysqli_query($conn, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}
+
+// Close connection
+mysqli_close($conn);
+?>
+
